e49380bd7bc7131aa1590af0f14c485ee358c19a
unknown <shahbaz_ali@outlook.in>
2023-08-31 02:29:28 +0530

    add error handleer

---
+++ bot_manager_server/src/utils/errorHandlerUtil.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+
+/**
+ * This is the error handler middleware it handles all the errors, If NODE_ENV is dev then it will send the stack trace
+ * @param err error object
+ * @param req Express.Request
+ * @param res Express.Response
+ * @param next Express.NextFunction
+ */
+const ErrorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
+    const errStatus: number = err.status || 500;
+    const errMsg: string = err.message || 'Something went wrong';
+    res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: process.env.NODE_ENV === 'dev' ? err.stack : {}
+    })
+}
+
+export { ErrorHandler };
