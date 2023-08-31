c2421c76c73135f9b6c9166435963d419fbb22b9
unknown <shahbaz_ali@outlook.in>
2023-08-31 16:28:31 +0530

    update the middleware

---
+++ bot_manager_server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import { DISCORD_BOT_SERVER_API_KEY } from '../config';
+import { APIError } from '../utils';
+
+/**
+ * This middleware validates the request by checking the API KEY
+ * @param req Express.Request
+ * @param res Express.Response
+ * @param next Express.NextFunction
+ */
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    // To-do : Add Admin Authentication system later on
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            const err: APIError = new Error('API Key not found');
+            err.status = 400;
+            throw err;
+        }
+        
+        if (token !== DISCORD_BOT_SERVER_API_KEY) {
+            const err: APIError = new Error('UnAuthorized: Incorrect API Key');
+            err.status = 401;
+            throw err;
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
